fee149fe435dc8d46125e88400009b6c06846a9d
KevinSimo2022 <116545828+KevinSimo2022@users.noreply.github.com>
2023-02-02 10:14:46 +0000

    hey

---
+++ 1-last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print('Last digit of', number, 'is', number[:-1] )
+if number[5:] < number[:-1] < 5:
+    if number[0:] == number[:-1] == 0: 
+        if 6 > number[:-1] != 0:
+            print('and is less than 6 and not 0')
