ee8af469e21eef12bf7dfaf96dc3f230347dde17
KevinSimo2022 <k.takoulo@alustudent.com>
2023-02-02 15:16:01 +0200

    Revert "hey"
    This reverts commit 950e4ca2af5222a490d23746b4b6b3d420da9267.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-LNum = abs(number) % 10
+LNum = number % 10
 if  number < 0:
     LNum = -LNum
-print('Last digit of' , number, 'is', LNum, 'and is end=')
 if LNum > 5:
     print('Last digit of', number, 'is', LNum, 'and is greater than 5')
 elif LNum == 0:
     print('Last digit of', number, 'is', LNum, 'and is 0')
+elif LNum < 6 and LNum != 0:
+    print('Last digit of', number, 'is', LNum, 'and is less than 6 and not 0')
