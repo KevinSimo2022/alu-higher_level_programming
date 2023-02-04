ffc8a3b8aefc733f5578481b7aed3121e195dd6c
KevinSimo2022 <k.takoulo@alustudent.com>
2023-02-04 02:07:35 -0800

    h

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for l in range(00, 99 + 1):
-    if l == 99:    
+    if l == 99:
         print("{}".format(l))
-    else:    
+    else: 
         print("{:02}".format(l), end=', ')
