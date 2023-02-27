8525221e0caa8455078a8f588325343664e6ae91
KevinSimo2022 <k.takoulo@alustudent.com>
2023-02-27 07:54:59 -0800

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        result = (0,None)
+        result = (0, None)
         return result
     else:
         res = (length, sentence[0:1])
