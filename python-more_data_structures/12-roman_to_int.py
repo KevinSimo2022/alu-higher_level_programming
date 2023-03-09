f6b1d7fbb473185c335aac8e0ae7e326d123fe1c
KevinSimo2022 <k.takoulo@alustudent.com>
2023-03-09 01:48:33 -0800

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-    if roman_string and type(roman_string) == str:
-        num = 0
-        for i in range(len(roman_string)):
-            for j in romans.keys():
-                if roman_string[:] is j:
-                    num = romans[j]
-                    return num
-                if roman_string[i:i+1] is j:
-                    num += romans[j]
-                    break
-        return num
-    else:
         return 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+    num = 0
+    num_list = list()
+    if len(roman_string) == 1:
+        return roman_numerals[roman_string]
+    if len(roman_string) > 1:
+        for i in range(0, len(roman_string)):
+            try:
+                if roman_string[i] == "I" and \
+                        (roman_string[i + 1] == "V" or
+                         roman_string[i + 1] == "X") \
+                        or roman_string[i] == "X" and \
+                        (roman_string[i + 1] == "C" or
+                         roman_string[i + 1] == "L") \
+                        or roman_string[i] == "C" and \
+                        (roman_string[i + 1] == "D" or
+                         roman_string[i + 1] == "M"):
+                    num_list.append(-int(roman_numerals[roman_string[i]]))
+                else:
+                    num_list.append(int(roman_numerals[roman_string[i]]))
+            except IndexError:
+                num_list.append(int(roman_numerals[roman_string[i]]))
+                pass
+    return sum(num_list)
