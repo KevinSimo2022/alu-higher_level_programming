4d61f86fdec15048d8750f251a4c1017987ed2de
KevinSimo2022 <ktakoulo@alustudent.com>
2023-04-10 15:30:52 +0000

    f

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Defines unittests for base.py.
-Unittest classes:
-    TestBase_instantiation - line 21
-    TestBase_to_json_string - line 108
-    TestBase_save_to_file - line 154
-    TestBase_from_json_string - line 232
-    TestBase_create - line 286
-    TestBase_load_from_file - line 338
-    TestBase_save_to_file_csv - line 404
-    TestBase_load_from_file_csv - line 482
+"""Unittest classes:
+    TestBase_instantiation - line 23
+    TestBase_to_json_string - line 110
+    TestBase_save_to_file - line 156
+    TestBase_from_json_string - line 234
+    TestBase_create - line 288
+    TestBase_load_from_file - line 340
+    TestBase_save_to_file_csv - line 406
+    TestBase_load_from_file_csv - line 484
 """
 import os
 import unittest
@@ -458,16 +457,6 @@ class TestBase_save_to_file_csv(unittest.TestCase):
         Square.save_to_file_csv([s])
         with open("Square.csv", "r") as f:
             self.assertTrue("8,10,7,2", f.read())
-
-    def test_save_to_file__csv_None(self):
-        Square.save_to_file_csv(None)
-        with open("Square.csv", "r") as f:
-            self.assertEqual("[]", f.read())
-
-    def test_save_to_file_csv_empty_list(self):
-        Square.save_to_file_csv([])
-        with open("Square.csv", "r") as f:
-            self.assertEqual("[]", f.read())
 
     def test_save_to_file_csv_no_args(self):
         with self.assertRaises(TypeError):
