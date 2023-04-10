be0c21a370a92bf62ad18ecb81e5e14919a8e827
KevinSimo2022 <ktakoulo@alustudent.com>
2023-04-10 08:46:17 +0000

    done

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3
 from models.base import Base
-""" Module that contains class Rectangle
-"""
-class Rectangle(Base):
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
+class Rectangle(Base):
+    # constructor for the Rectangle class
+    def __init__(self, width, height, x=0, y=0, id=None):
+        # call the constructor of the Base class with the id argument
+        super().__init__(id)
+        # assign the width, height, x, and y arguments to their respective attributes
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    # getter and setter for the width attribute
     @property
     def width(self):
         return self.__width
@@ -18,6 +20,7 @@ class Rectangle(Base):
     def width(self, value):
         self.__width = value
 
+    # getter and setter for the height attribute
     @property
     def height(self):
         return self.__height
@@ -26,6 +29,7 @@ class Rectangle(Base):
     def height(self, value):
         self.__height = value
 
+    # getter and setter for the x attribute
     @property
     def x(self):
         return self.__x
@@ -34,6 +38,7 @@ class Rectangle(Base):
     def x(self, value):
         self.__x = value
 
+    # getter and setter for the y attribute
     @property
     def y(self):
         return self.__y
